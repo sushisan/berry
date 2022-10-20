f0da7536eca4894c2b32043989aae9a376471387
egonzalez <egonzalez@msi.notebook.hiperion.local>
2022-10-20 10:12:35 -0300

    Disable markdown

--- docs/conf.py
+++ docs/conf.py
@@ -31,7 +31,6 @@ release = '1.1.0'
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
-        'myst_parser',
         'sphinx.ext.autodoc',
         'sphinx.ext.doctest',
         'sphinx.ext.mathjax',
@@ -98,7 +97,6 @@ if not on_rtd:  # only import and set the theme if we're building docs locally
 # html_static_path = ['_static']
 
 source_suffix = {
-     '.md': 'markdown',
     '.rst': 'restructuredtext',
 }
 
