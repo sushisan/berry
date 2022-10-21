fdc2a34958d99a9c61a172acffc49128048b7770
egonzalez <egonzalez@msi.notebook.hiperion.local>
2022-10-21 18:34:17 -0300

    .

--- docs/conf.py
+++ docs/conf.py
@@ -1,8 +1,35 @@
-project = 'The Berry Script Language'
+# Configuration file for the Sphinx documentation builder.
+#
+# This file only contains a selection of the most common options. For a full
+# list see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Path setup --------------------------------------------------------------
+
+# If extensions (or modules to document with autodoc) are in another directory,
+# add these directories to sys.path here. If the directory is relative to the
+# documentation root, use os.path.abspath to make it absolute, like shown here.
+#
+# import os
+# import sys
+# sys.path.insert(0, os.path.abspath('.'))
+
+
+# -- Project information -----------------------------------------------------
+
+project = 'berry'
 copyright = '2022, Guan Wenliang & Stephan Hadinger'
 author = 'Guan Wenliang & Stephan Hadinger'
+
+# The full version, including alpha/beta/rc tags
 release = '1.1.0'
 
+
+# -- General configuration ---------------------------------------------------
+
+# Add any Sphinx extension module names here, as strings. They can be
+# extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
+# ones.
 extensions = [
         'sphinx.ext.autodoc',
         'sphinx.ext.doctest',
@@ -12,9 +39,16 @@ extensions = [
         'sphinx.ext.todo',
         'breathe'
 ]
+
+# Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
+
+# List of patterns, relative to source directory, that match files and
+# directories to ignore when looking for source files.
+# This pattern also affects html_static_path and html_extra_path.
 exclude_patterns = ['conf.py', 'Doxyfile']
 
+# on_rtd is whether we are on readthedocs.org
 import os
 on_rtd = os.environ.get('READTHEDOCS', None) == 'True'
 
@@ -22,18 +56,30 @@ import subprocess
 subprocess.call('doxygen', shell=True)
 
 
+# -- Options for HTML output -------------------------------------------------
+
 import sphinx_typo3_theme
 
+# The theme to use for HTML and HTML Help pages.  See the documentation for
+# a list of builtin themes.
+#
 html_theme = 'sphinx_typo3_theme'
 html_logo = '../berry-logo.png'
+    
+# Theme options are theme-specific and customize the look and feel of a theme
+# further.  For a list of options available for each theme, see the
+# documentation.
+#
 html_theme_options = {
-'logo_title' : 'The Berry Script Language',
-'logo_alt' : 'Berry',
-'logo_url' : 'https://github.com/berry-lang/berry'
+   'logo_title' : 'The Berry Script Language',
+   'logo_alt' : 'Berry',
+   'logo_url' : 'https://github.com/berry-lang/berry'
+}
 
 source_suffix = {
     '.rst': 'restructuredtext',
 }
+
 # Breathe Configuration
 breathe_projects = {"berry" : "../src"}
 breathe_default_project = "berry"
