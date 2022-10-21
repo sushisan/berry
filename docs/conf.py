0ab5ecdabe874e4226433ab89a694ffd8d53fb96
egonzalez <egonzalez@msi.notebook.hiperion.local>
2022-10-21 18:29:48 -0300

    orrect conf

--- docs/conf.py
+++ docs/conf.py
@@ -1,35 +1,8 @@
-# Configuration file for the Sphinx documentation builder.
-#
-# This file only contains a selection of the most common options. For a full
-# list see the documentation:
-# https://www.sphinx-doc.org/en/master/usage/configuration.html
-
-# -- Path setup --------------------------------------------------------------
-
-# If extensions (or modules to document with autodoc) are in another directory,
-# add these directories to sys.path here. If the directory is relative to the
-# documentation root, use os.path.abspath to make it absolute, like shown here.
-#
-# import os
-# import sys
-# sys.path.insert(0, os.path.abspath('.'))
-
-
-# -- Project information -----------------------------------------------------
-
-project = 'berry'
+project = 'The Berry Script Language'
 copyright = '2022, Guan Wenliang & Stephan Hadinger'
 author = 'Guan Wenliang & Stephan Hadinger'
-
-# The full version, including alpha/beta/rc tags
 release = '1.1.0'
 
-
-# -- General configuration ---------------------------------------------------
-
-# Add any Sphinx extension module names here, as strings. They can be
-# extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
-# ones.
 extensions = [
         'sphinx.ext.autodoc',
         'sphinx.ext.doctest',
@@ -39,16 +12,9 @@ extensions = [
         'sphinx.ext.todo',
         'breathe'
 ]
-
-# Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
-
-# List of patterns, relative to source directory, that match files and
-# directories to ignore when looking for source files.
-# This pattern also affects html_static_path and html_extra_path.
 exclude_patterns = ['conf.py', 'Doxyfile']
 
-# on_rtd is whether we are on readthedocs.org
 import os
 on_rtd = os.environ.get('READTHEDOCS', None) == 'True'
 
@@ -56,56 +22,18 @@ import subprocess
 subprocess.call('doxygen', shell=True)
 
 
-# -- Options for HTML output -------------------------------------------------
+import sphinx_typo3_theme
 
-if not on_rtd:  # only import and set the theme if we're building docs locally
-    #import sphinx_rtd_theme
-    import sphinx_typo3_theme
-
-    # The theme to use for HTML and HTML Help pages.  See the documentation for
-    # a list of builtin themes.
-    #
-    html_theme = 'sphinx_typo3_theme'
-    html_logo = '../berry-logo.png'
-    #html_logo_alt = 'Berry'
-    #html_logo_url = 'https://github.com/berry-lang/berry'
-    
-    # Theme options are theme-specific and customize the look and feel of a theme
-    # further.  For a list of options available for each theme, see the
-    # documentation.
-    #
-    html_theme_options = {
-    'logo_title' : 'The Berry Script Language',
-    'logo_alt' : 'Berry',
-    'logo_url' : 'https://github.com/berry-lang/berry'
-    }
-
-    # Add any paths that contain custom static files (such as style sheets) here,
-    # relative to this directory. They are copied after the builtin static files,
-    # so a file named "default.css" will overwrite the builtin "default.css".
-    # html_static_path = ['_static']
-
-    # Custom sidebar templates, must be a dictionary that maps document names
-    # to template names.
-    #
-    # The default sidebars (for documents that don't match any pattern) are
-    # defined by theme itself.  Builtin themes are using these templates by
-    # default: ``['localtoc.html', 'relations.html', 'sourcelink.html',
-    # 'searchbox.html']``.
-    #
-    # html_sidebars = {}
-
-# otherwise, readthedocs.org uses their theme by default, so no need to specify it
-
-# Add any paths that contain custom static files (such as style sheets) here,
-# relative to this directory. They are copied after the builtin static files,
-# so a file named "default.css" will overwrite the builtin "default.css".
-# html_static_path = ['_static']
+html_theme = 'sphinx_typo3_theme'
+html_logo = '../berry-logo.png'
+html_theme_options = {
+'logo_title' : 'The Berry Script Language',
+'logo_alt' : 'Berry',
+'logo_url' : 'https://github.com/berry-lang/berry'
 
 source_suffix = {
     '.rst': 'restructuredtext',
 }
-
 # Breathe Configuration
 breathe_projects = {"berry" : "../src"}
 breathe_default_project = "berry"
